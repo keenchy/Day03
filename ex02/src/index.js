3534e307dcdecaa6162e57f6f7db85129f213533
keenchy <h.kenan81@hotmail.com>
2021-03-10 22:16:59 +0100

    inserting single line and multi-line comments

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+//This is a single line JavaScript comment
+//This is a single line JavaScript comment
+//This is a single line JavaScript comment
+//This is a single line JavaScript comment
+//This is a single line JavaScript comment
+
+/*document.write("You can't see multi-line comments!");
+document.write("You can't see multi-line comments!");
+document.write("You can't see multi-line comments!");
+document.write("You can't see multi-line comments!");
+document.write("You can't see multi-line comments!");*/
