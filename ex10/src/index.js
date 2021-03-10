42657561f839a177fde9d352b899b0d82cfa0546
keenchy <h.kenan81@hotmail.com>
2021-03-11 00:07:17 +0100

    adding new .js file

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var difference = numOne - numTwo;
+    //Only Only change code below this line
+
+    {
+        return difference;
+
+    }
+    console.log(main(15, 8));
+    console.log(main(25, 8));
+    console.log(main(425, 120));
+
+    module.exports = main;
