0481806a9eb56fbdcce2807b6d8b9be471162ad1
keenchy <h.kenan81@hotmail.com>
2021-03-11 01:21:14 +0100

    update index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,13 @@
-var x = x + 8;
+function main() {
+
+    // Only change code below this line
+    var x = 8;
+
+    // Only change code below this line
+
+    return x;
+
+}
+
+console.log(main());
+module.exports = main;
