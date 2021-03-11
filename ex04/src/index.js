d25c4bb6dbe0d25280120f269998436484f3bb92
keenchy <h.kenan81@hotmail.com>
2021-03-11 01:04:16 +0100

    modified index.js

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,3 +1,11 @@
-var x = 10;
-var = 10;
-var x + y = 20;
+function main() {
+    //Only change code below this line
+    var x = 10;
+    var y = x;
+    return [x, y];
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
